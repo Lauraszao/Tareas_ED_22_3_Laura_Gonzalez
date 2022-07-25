95b2752c2a317352bc4ee3f4f94a174ea694cc76
Lauraszao <106337936+Lauraszao@users.noreply.github.com>
2022-07-24 22:02:39 -0500

    cambios

--- Assets/Script/Move2.cs
+++ Assets/Script/Move2.cs
@@ -7,6 +7,13 @@ public class Move2 : MonoBehaviour
  
   
     private Rigidbody2D rb;
+
+ 
+    //Mueve este GameObject 2 unidades por segundo en la dirección de avance
+     private float speed = 2f;
+
+    //Se le asign una fuerza de salto
+    public float jumpForce = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +23,18 @@ public class Move2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+     //Se le asigna la tecla Space
       if(Input.GetKeyUp(KeyCode.Space))
       {
-       rb.AddForce(Vector3.right*5,ForceMode2D.Impulse);
-      }
-      rb.velocity=new Vector2(0.5f,rb.velocity.y);
+        //Tiene una fuerza e impulso de salto
+       rb.AddForce(Vector3.up*jumpForce,ForceMode2D.Impulse);
+      }//Se le asigna velocidad en dirección a y
+      rb.velocity=new Vector2(5f,rb.velocity.y);
+
+       transform.Translate(Vector3.forward * Time.deltaTime * speed);
+       { 
+        speed = speed * -1; 
+    } 
       
     }
 
