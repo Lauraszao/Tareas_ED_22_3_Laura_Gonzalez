c3c5e740bd0710fc688228970a7393bb4454c5be
Lauraszao <106337936+Lauraszao@users.noreply.github.com>
2022-07-24 20:17:48 -0500

    cambios

--- Assets/Script/Move2.cs
+++ Assets/Script/Move2.cs
@@ -5,40 +5,25 @@ using UnityEngine;
 public class Move2 : MonoBehaviour
 {
  
-    Rigidbody m_Rigidbody;
-    public float m_Thrust = 20f;
-    public float velocidad = 0.5f;
-    public float fuerzaSalto;
-    private Rigidbody2D rigidBody;
+  
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        m_Rigidbody = GetComponent<Rigidbody>();
-        transform.position = new Vector3(-7.44f,4.14f,0f);
-        rigidBody = GetComponent<Rigidbody2D>();
+      rb=GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-      if(Input.GetKey(KeyCode.A))
+      if(Input.GetKeyUp(KeyCode.Space))
       {
-        transform.position += Vector3.left * velocidad * Time.deltaTime;
-      } 
-      if(Input.GetKey(KeyCode.D))
-      {
-        transform.position += Vector3.right * velocidad * Time.deltaTime;
-      } 
-      ProcesarSalto();
+       rb.AddForce(Vector3.right*5,ForceMode2D.Impulse);
+      }
+      rb.velocity=new Vector2(0.5f,rb.velocity.y);
+      
     }
 
-    void ProcesarSalto()
-    {
-      if(Input.GetKeyDown(KeyCode.W))
-      {
-        rigidBody.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
-      }
-    }
     
 
 }
