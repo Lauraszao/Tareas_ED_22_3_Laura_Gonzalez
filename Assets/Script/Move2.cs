bb1d0b4ddde248f98f8dd9003089a748d379b693
Lauraszao <106337936+Lauraszao@users.noreply.github.com>
2022-08-03 20:37:10 -0500

    cambios

--- Assets/Script/Move2.cs
+++ Assets/Script/Move2.cs
@@ -4,40 +4,34 @@ using UnityEngine;
 
 public class Move2 : MonoBehaviour
 {
- 
-  
-    private Rigidbody2D rb;
+   public Rigidbody2D  rb; 
 
- 
-    //Mueve este GameObject 2 unidades por segundo en la dirección de avance
-     private float speed = 2f;
-
-    //Se le asign una fuerza de salto
-    public float jumpForce = 10;
+   public float speed;
+    
+    public float jumpForce = 5;
     // Start is called before the first frame update
     void Start()
     {
-      rb=GetComponent<Rigidbody2D>();
+        rb=GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-     //Se le asigna la tecla Space
-      if(Input.GetKeyUp(KeyCode.Space))
-      {
-        //Tiene una fuerza e impulso de salto
-       rb.AddForce(Vector3.up*jumpForce,ForceMode2D.Impulse);
-      }//Se le asigna velocidad en dirección a y
-      rb.velocity=new Vector2(5f,rb.velocity.y);
+        if(Input.GetKeyUp(KeyCode.W)){
+            rb.AddForce(Vector3.up*jumpForce,ForceMode2D.Impulse);
+        }
 
-       transform.Translate(Vector3.forward * Time.deltaTime * speed);
-       { 
-        speed = speed * -1; 
-    } 
-      
+
+       if (Input.GetKey(KeyCode.D))
+       {
+           rb.velocity = new Vector2(speed,rb.velocity.y);
+       }
+
+          if (Input.GetKey(KeyCode.A))
+       {
+           rb.velocity = new Vector2(-speed,rb.velocity.y);
+       }
+     
     }
-
-    
-
 }
