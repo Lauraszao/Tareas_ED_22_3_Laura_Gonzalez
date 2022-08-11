908f537a63d1e0f563437fca6d8d25a1d886bb48
Lauraszao <gslaurita@gmail.com>
2022-08-11 12:55:09 -0500

    cambios

--- Assets/Script/Move2.cs
+++ Assets/Script/Move2.cs
@@ -1,3 +1,14 @@
+//Nombre del desarrollador: Laura Gonzalez Sandoval
+//Asignatura: Estructura de datos
+//Profesor: Josué Israel Rivas Diaz
+//Fuente en la que base el script: De las clases del profesor Josué Rivas 
+//Descripción: En este script, se le va a asignar una variable publica de tipo rigidboddy2d, luego se le asigana una
+//variable publica de tipo float para poder cambiar la velocidad de unity
+//se le asigna una fuerza de salto. se coloca condicionante, si presiono la tecla w mi personaje salta
+//se coloca condicionante, si presiono la tecla D mi personaje se mueve a la derecha
+// se usan dos vectores x y y , en la cual la velocidad y el rb se le asigna a la direccion y o sea  ala derecha
+// //rb por la velocidad es igual a vector dos , donde la celocidad es negativa es decir, va en direccion izquiersa usando dos vectores
+       
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
